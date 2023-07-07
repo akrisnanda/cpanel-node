d824ab5b2f97bf4a10998c4beebf1039623ac081
akrisnanda <44166275+akrisnanda@users.noreply.github.com>
2023-07-07 13:48:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var http = require('http');
+vart http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = '<h3>auto deploy...</h3>\nAlhamdulillah, It works from github! \n',
